c54ab8de9dc7dc6112445460ce5887433fa19746
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-04-05 00:46:31 +0800

    frontend for view movies table

---
+++ src/components/MoviesTable/MoviesTable.jsx
@@ -0,0 +1,267 @@
+const MoviesTable = () => {
+  return (
+    <div class="container max-w-5xl px-4 mx-auto sm:px-8">
+      <div class="py-8">
+        <div class="px-4 py-4 -mx-4 overflow-x-auto sm:-mx-8 sm:px-8">
+          <div class="inline-block min-w-full overflow-hidden rounded-lg shadow">
+            <table class="min-w-full leading-normal">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    class="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
+                  >
+                    ID
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
+                  >
+                    TITLE
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
+                  >
+                    YEAR
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
+                  >
+                    GENRE
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
+                  >
+                    DIRECTOR
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
+                  >
+                    ACTOR&nbsp;1
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
+                  >
+                    ACTOR&nbsp;2
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">410928</p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">
+                      What&apos;s Up, America?
+                    </p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">1979</p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">Action</p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">
+                      Laurence Starkman
+                    </p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">
+                      Deborah Harmon
+                    </p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">Lisa Ruddy</p>
+                  </td>
+                </tr>
+                <tr>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">410928</p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">
+                      What&apos;s Up, America?
+                    </p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">1979</p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">Action</p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">
+                      Laurence Starkman
+                    </p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">
+                      Deborah Harmon
+                    </p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">Lisa Ruddy</p>
+                  </td>
+                </tr>
+                <tr>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">410928</p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">
+                      What&apos;s Up, America?
+                    </p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">1979</p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">Action</p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">
+                      Laurence Starkman
+                    </p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">
+                      Deborah Harmon
+                    </p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">Lisa Ruddy</p>
+                  </td>
+                </tr>
+                <tr>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">410928</p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">
+                      What&apos;s Up, America?
+                    </p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">1979</p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">Action</p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">
+                      Laurence Starkman
+                    </p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">
+                      Deborah Harmon
+                    </p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">Lisa Ruddy</p>
+                  </td>
+                </tr>
+                <tr>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">410928</p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">
+                      What&apos;s Up, America?
+                    </p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">1979</p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">Action</p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">
+                      Laurence Starkman
+                    </p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">
+                      Deborah Harmon
+                    </p>
+                  </td>
+                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                    <p class="text-gray-900 whitespace-no-wrap">Lisa Ruddy</p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <div class="flex flex-col items-center px-5 py-5 bg-white xs:flex-row xs:justify-between">
+              <div class="flex items-center">
+                <button
+                  type="button"
+                  class="w-full p-4 text-base text-gray-600 bg-white border rounded-l-xl hover:bg-gray-100"
+                >
+                  <svg
+                    width="9"
+                    fill="currentColor"
+                    height="8"
+                    class=""
+                    viewBox="0 0 1792 1792"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z"></path>
+                  </svg>
+                </button>
+                <button
+                  type="button"
+                  class="w-full px-4 py-2 text-base text-indigo-500 bg-white border-t border-b hover:bg-gray-100 "
+                >
+                  1
+                </button>
+                <button
+                  type="button"
+                  class="w-full px-4 py-2 text-base text-gray-600 bg-white border hover:bg-gray-100"
+                >
+                  2
+                </button>
+                <button
+                  type="button"
+                  class="w-full px-4 py-2 text-base text-gray-600 bg-white border-t border-b hover:bg-gray-100"
+                >
+                  3
+                </button>
+                <button
+                  type="button"
+                  class="w-full px-4 py-2 text-base text-gray-600 bg-white border hover:bg-gray-100"
+                >
+                  4
+                </button>
+                <button
+                  type="button"
+                  class="w-full p-4 text-base text-gray-600 bg-white border-t border-b border-r rounded-r-xl hover:bg-gray-100"
+                >
+                  <svg
+                    width="9"
+                    fill="currentColor"
+                    height="8"
+                    class=""
+                    viewBox="0 0 1792 1792"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z"></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MoviesTable;
