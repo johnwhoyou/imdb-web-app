00df9bcf9c5ba293f726f1a66c576bd38cbae804
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-04-05 15:55:36 +0800

    removed SearchMovie component in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import MoviesTable from "@/components/MoviesTable/MoviesTable";
-import SearchMovie from "@/components/SearchMovie/SearchMovie";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,7 +8,6 @@ export default function Home() {
   return (
     <>
       <main className="pt-16">
-        <SearchMovie />
         <MoviesTable />
       </main>
     </>
