8da4917cac9ce08427eb6a31a5b00142a61b1f9a
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-04-05 17:29:26 +0800

    removed main padding

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <main className="pt-16">
+      <main>
         <MoviesTable />
       </main>
     </>
