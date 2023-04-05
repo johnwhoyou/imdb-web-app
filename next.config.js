78bcb9093eb243aa1774ea792e4fe511f38b3f32
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-04-05 17:29:17 +0800

    added IMDB logo and responsive NavBar

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["cdn-icons-png.flaticon.com"],
+  },
 };
 
 module.exports = nextConfig;
