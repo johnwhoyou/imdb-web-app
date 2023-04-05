449c63736f99b8ce37faf4f37dfad6b57ee27524
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-04-05 14:08:35 +0800

    MoviesTable now fetches from the DB

--- src/components/MoviesTable/MoviesTable.jsx
+++ src/components/MoviesTable/MoviesTable.jsx
@@ -1,215 +1,141 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 const MoviesTable = () => {
+  const [movies, setMovies] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const rowsPerPage = 10;
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await axios.get(
+          `/api/movies?page=${currentPage}&limit=${rowsPerPage}`
+        );
+        setMovies(response.data.data);
+        setTotalPages(response.data.totalPages);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
+    };
+
+    fetchMovies();
+  }, [currentPage]);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
-    <div class="container max-w-5xl px-4 mx-auto sm:px-8">
-      <div class="py-8">
-        <div class="px-4 py-4 -mx-4 overflow-x-auto sm:-mx-8 sm:px-8">
-          <div class="inline-block min-w-full overflow-hidden rounded-lg shadow">
-            <table class="min-w-full leading-normal">
+    <div className="container max-w-5xl px-4 mx-auto sm:px-8">
+      <div className="py-8">
+        <div className="px-4 py-4 -mx-4 overflow-x-auto sm:-mx-8 sm:px-8">
+          <div className="inline-block min-w-full overflow-hidden rounded-lg shadow">
+            <table className="min-w-full leading-normal">
               <thead>
                 <tr>
                   <th
                     scope="col"
-                    class="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
+                    className="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
                   >
                     ID
                   </th>
                   <th
                     scope="col"
-                    class="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
+                    className="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
                   >
                     TITLE
                   </th>
                   <th
                     scope="col"
-                    class="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
+                    className="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
                   >
                     YEAR
                   </th>
                   <th
                     scope="col"
-                    class="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
+                    className="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
                   >
                     GENRE
                   </th>
                   <th
                     scope="col"
-                    class="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
+                    className="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
                   >
                     DIRECTOR
                   </th>
                   <th
                     scope="col"
-                    class="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
+                    className="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
                   >
                     ACTOR&nbsp;1
                   </th>
                   <th
                     scope="col"
-                    class="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
+                    className="px-5 py-3 text-sm font-semibold text-left text-gray-800 uppercase bg-white border-b border-gray-200"
                   >
                     ACTOR&nbsp;2
                   </th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">410928</p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">
-                      What&apos;s Up, America?
-                    </p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">1979</p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">Action</p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">
-                      Laurence Starkman
-                    </p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">
-                      Deborah Harmon
-                    </p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">Lisa Ruddy</p>
-                  </td>
-                </tr>
-                <tr>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">410928</p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">
-                      What&apos;s Up, America?
-                    </p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">1979</p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">Action</p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">
-                      Laurence Starkman
-                    </p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">
-                      Deborah Harmon
-                    </p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">Lisa Ruddy</p>
-                  </td>
-                </tr>
-                <tr>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">410928</p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">
-                      What&apos;s Up, America?
-                    </p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">1979</p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">Action</p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">
-                      Laurence Starkman
-                    </p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">
-                      Deborah Harmon
-                    </p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">Lisa Ruddy</p>
-                  </td>
-                </tr>
-                <tr>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">410928</p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">
-                      What&apos;s Up, America?
-                    </p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">1979</p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">Action</p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">
-                      Laurence Starkman
-                    </p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">
-                      Deborah Harmon
-                    </p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">Lisa Ruddy</p>
-                  </td>
-                </tr>
-                <tr>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">410928</p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">
-                      What&apos;s Up, America?
-                    </p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">1979</p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">Action</p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">
-                      Laurence Starkman
-                    </p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">
-                      Deborah Harmon
-                    </p>
-                  </td>
-                  <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p class="text-gray-900 whitespace-no-wrap">Lisa Ruddy</p>
-                  </td>
-                </tr>
+                {console.log(movies)}
+                {movies &&
+                  movies.map((movie) => (
+                    <tr key={movie.id}>
+                      <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                        <p className="text-gray-900 whitespace-no-wrap">
+                          {movie.id}
+                        </p>
+                      </td>
+                      <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                        <p className="text-gray-900 whitespace-no-wrap">
+                          {movie.name ? movie.name : "N/D"}
+                        </p>
+                      </td>
+                      <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                        <p className="text-gray-900 whitespace-no-wrap">
+                          {movie.year}
+                        </p>
+                      </td>
+                      <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                        <p className="text-gray-900 whitespace-no-wrap">
+                          {movie.genre ? movie.genre : "N/D"}
+                        </p>
+                      </td>
+                      <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                        <p className="text-gray-900 whitespace-no-wrap">
+                          {movie.director ? movie.director : "N/D"}
+                        </p>
+                      </td>
+                      <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                        <p className="text-gray-900 whitespace-no-wrap">
+                          {movie.actor1 ? movie.actor1 : "N/D"}
+                        </p>
+                      </td>
+                      <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                        <p className="text-gray-900 whitespace-no-wrap">
+                          {movie.actor2 ? movie.actor2 : "N/D"}
+                        </p>
+                      </td>
+                    </tr>
+                  ))}
               </tbody>
             </table>
-            <div class="flex flex-col items-center px-5 py-5 bg-white xs:flex-row xs:justify-between">
-              <div class="flex items-center">
+            <div className="flex flex-col items-center px-5 py-5 bg-white xs:flex-row xs:justify-between">
+              <div className="flex items-center">
                 <button
                   type="button"
-                  class="w-full p-4 text-base text-gray-600 bg-white border rounded-l-xl hover:bg-gray-100"
+                  className="w-full p-4 text-base text-gray-600 bg-white border rounded-l-xl hover:bg-gray-100"
+                  onClick={() => handlePageChange(currentPage - 1)}
+                  disabled={currentPage === 1}
                 >
                   <svg
                     width="9"
                     fill="currentColor"
                     height="8"
-                    class=""
+                    className=""
                     viewBox="0 0 1792 1792"
                     xmlns="http://www.w3.org/2000/svg"
                   >
@@ -218,37 +144,21 @@ const MoviesTable = () => {
                 </button>
                 <button
                   type="button"
-                  class="w-full px-4 py-2 text-base text-indigo-500 bg-white border-t border-b hover:bg-gray-100 "
+                  className="w-full px-4 py-2 text-base text-indigo-500 bg-white border"
                 >
-                  1
+                  {currentPage}
                 </button>
                 <button
                   type="button"
-                  class="w-full px-4 py-2 text-base text-gray-600 bg-white border hover:bg-gray-100"
-                >
-                  2
-                </button>
-                <button
-                  type="button"
-                  class="w-full px-4 py-2 text-base text-gray-600 bg-white border-t border-b hover:bg-gray-100"
-                >
-                  3
-                </button>
-                <button
-                  type="button"
-                  class="w-full px-4 py-2 text-base text-gray-600 bg-white border hover:bg-gray-100"
-                >
-                  4
-                </button>
-                <button
-                  type="button"
-                  class="w-full p-4 text-base text-gray-600 bg-white border-t border-b border-r rounded-r-xl hover:bg-gray-100"
+                  className="w-full p-4 text-base text-gray-600 bg-white border-t border-b border-r rounded-r-xl hover:bg-gray-100"
+                  onClick={() => handlePageChange(currentPage + 1)}
+                  disabled={currentPage === totalPages}
                 >
                   <svg
                     width="9"
                     fill="currentColor"
                     height="8"
-                    class=""
+                    className=""
                     viewBox="0 0 1792 1792"
                     xmlns="http://www.w3.org/2000/svg"
                   >
