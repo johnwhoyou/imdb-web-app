9bb24658d338523141a910de5ba81d2a46b4feed
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-04-05 17:44:49 +0800

    made /search the index route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
-import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { Inter } from "next/font/google";
 import MoviesTable from "@/components/MoviesTable/MoviesTable";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <>
-      <main>
-        <MoviesTable />
-      </main>
-    </>
-  );
+  const router = useRouter();
+
+  // Search Movie will be our index page
+  useEffect(() => {
+    router.push("/search");
+  });
+
+  return null;
 }
---
+++ src/pages/search/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MoviesTable from "@/components/MoviesTable/MoviesTable";
+
+function App() {
+  return <MoviesTable />;
+}
+
+export default App;
