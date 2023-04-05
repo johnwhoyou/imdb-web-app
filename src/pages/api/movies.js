9abbbefb9445c3a742bd3254fb45d1165ead1754
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-04-05 14:09:08 +0800

    edited movies api to support pagination

--- src/pages/api/movies.js
+++ src/pages/api/movies.js
@@ -2,9 +2,27 @@ import Movie from "../../lib/models/Movie";
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 10;
+    const offset = (page - 1) * limit;
+
     try {
-      const movies = await Movie.findAll();
-      res.status(200).json(movies);
+      const movies = await Movie.findAndCountAll({
+        limit,
+        offset,
+        order: [["id", "ASC"]],
+      });
+
+      const totalPages = Math.ceil(movies.count / limit);
+
+      res.status(200).json({
+        data: movies.rows,
+        pagination: {
+          currentPage: page,
+          totalPages,
+          limit,
+        },
+      });
     } catch (error) {
       console.error("Error fetching movies:", error);
       res.status(500).json({ message: "Error fetching movies." });
