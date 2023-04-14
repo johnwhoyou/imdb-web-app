3edf4952224c7f07a27a9ea049d2ef227bbd67e5
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-04-14 23:42:44 +0800

    NavBar toggleMenu automatic close

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -59,11 +59,12 @@ const NavBar = () => {
             <li>
               <Link
                 href="/search"
+                onClick={toggleMenu}
                 className={
                   router.pathname === "/search"
                     ? `block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent`
                     : `block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`
-                } 
+                }
                 aria-current="page"
               >
                 Search
@@ -72,6 +73,7 @@ const NavBar = () => {
             <li>
               <Link
                 href="/insert"
+                onClick={toggleMenu}
                 className={
                   router.pathname === "/insert"
                     ? `block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent`
