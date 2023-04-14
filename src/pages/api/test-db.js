a258cedc75e2fd46212276bf945ee1ef4ef9519a
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-04-14 19:28:54 +0800

    changed test-db to adapt to new changes

--- src/pages/api/test-db.js
+++ src/pages/api/test-db.js
@@ -1,14 +1,46 @@
-import centralNode from "../../lib/db";
+import { allMoviesNode1, allMoviesNode2, allMoviesNode3 } from "../../lib/db";
 
-// File to Test DB Connection
+// File to Test DB Connections
 export default async function handler(req, res) {
+  let node1Status = "";
+  let node2Status = "";
+  let node3Status = "";
+
   try {
-    await centralNode.authenticate();
-    res
-      .status(200)
-      .json({ message: "Connection has been established successfully." });
+    await allMoviesNode1.authenticate();
+    node1Status = "Database 1 connection has been established successfully.";
+    console.log("Database 1 connection has been established successfully.");
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
-    res.status(500).json({ message: "Unable to connect to the database." });
+    node1Status = "Database 1 connection has been established successfully." + error;
+    console.error("Unable to connect to Database 1:", error);
   }
+
+  try {
+    await allMoviesNode2.authenticate();
+    node2Status = "Database 2 connection has been established successfully.";
+    console.log("Database 2 connection has been established successfully.");
+  } catch (error) {
+    node2Status = "Database 2 connection has been established successfully." + error;
+    console.error("Unable to connect to Database 2:", error);
+  }
+
+  try {
+    await allMoviesNode3.authenticate();
+    node3Status = "Database 3 connection has been established successfully.";
+    console.log("Database 3 connection has been established successfully.");
+  } catch (error) {
+    node3Status = "Database 3 connection has been established successfully." + error;
+    console.error("Unable to connect to Database 3:", error);
+  }
+
+  // Send a response with the results of the connection tests
+  const connectionStatuses = [
+    { database: 1, connected: node1Status },
+    { database: 2, connected: node2Status },
+    { database: 3, connected: node3Status },
+  ];
+
+  res
+    .status(200)
+    .json({ message: "Database connection test results.", connectionStatuses });
 }
