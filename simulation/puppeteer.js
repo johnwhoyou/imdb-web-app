759928b3f54b9fef6221f5e91dc8d593a5f9d8b9
zen-young <renzcruz50@gmail.com>
2023-04-16 16:29:18 +0800

    enabled all test scripts

--- simulation/puppeteer.js
+++ simulation/puppeteer.js
@@ -1,9 +1,11 @@
 const { caseOneTest } = require("./CaseOneTest");
 const { caseTwoTest } = require("./CaseTwoTest");
+const { caseThreeTest } = require("./CaseThreeTest");
 
 const runTests = async () => {
   await caseOneTest();
   await caseTwoTest();
+  await caseThreeTest();
 };
 
 runTests();
