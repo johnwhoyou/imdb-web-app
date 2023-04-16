747343569631f52c33717bd90fa77b23488e98cf
zen-young <renzcruz50@gmail.com>
2023-04-16 15:22:19 +0800

    fully implemented Case 2 test script

--- simulation/CaseTwoTest.js
+++ simulation/CaseTwoTest.js
@@ -5,13 +5,22 @@ const caseTwoTest = async () => {
   const cluster = await Cluster.launch({
     concurrency: Cluster.CONCURRENCY_CONTEXT,
     maxConcurrency: 3,
-    puppeteerOptions: { headless: false },
+    puppeteerOptions: { headless: true },
   });
 
+  const sampleMovieNames = [
+    "Test Movie",
+    "Updated Movie",
+    "This is a Sample Title",
+    "STADVDB Best Class",
+    "Hello World!",
+    "Distributed Databases: Greatest Fear of CS-ST Students",
+  ];
   const dataItems = [];
   let updateResult = "No write occured.";
+  let selectedName = "No movie name selected";
 
-  await cluster.execute(
+  await cluster.queue(
     "https://imdb.johnjoyo.dev/api/movies?movie_id=0e9c3edf-7dc4-47af-accb-76a796e8d8b3",
     async ({ data: url }) => {
       const movieData = await axios
@@ -21,32 +30,66 @@ const caseTwoTest = async () => {
     }
   );
 
-  await cluster.execute(
+  await cluster.queue(
     "https://imdb.johnjoyo.dev/update/0e9c3edf-7dc4-47af-accb-76a796e8d8b3",
     async ({ page, data: url }) => {
       await page.goto(url);
 
+      const movieName =
+        sampleMovieNames[Math.floor(Math.random() * sampleMovieNames.length)];
+      selectedName = movieName;
+
       await page.waitForXPath("/html/body/div/div/div/div[2]/form/input");
-      const movieTitleInput = (await page.$x("/html/body/div/div/div/div[2]/form/input"))[0];
+      const movieTitleInput = (
+        await page.$x("/html/body/div/div/div/div[2]/form/input")
+      )[0];
       await movieTitleInput.focus();
-      await page.keyboard.type("Updated Movie");
+      await page.keyboard.type(selectedName);
 
-      await page.waitForXPath("/html/body/div/div/div/div[2]/form/div[1]/section/input");
-      const yearInput = (await page.$x("/html/body/div/div/div/div[2]/form/div[1]/section/input"))[0];
+      await page.waitForXPath(
+        "/html/body/div/div/div/div[2]/form/div[1]/section/input"
+      );
+      const yearInput = (
+        await page.$x("/html/body/div/div/div/div[2]/form/div[1]/section/input")
+      )[0];
       await yearInput.focus();
-      await page.keyboard.type("2059");
-    
-      await page.waitForXPath("/html/body/div/div/div/div[2]/form/div[2]/section[1]/select");
-      const genreDropdown = (await page.$x("/html/body/div/div/div/div[2]/form/div[2]/section[1]/select"))[0];
-      await genreDropdown.click();
-    //   await page.waitForXPath("/html/body/div/div/div/div[2]/form/div[2]/section[1]/select/option[17]");
-    //   const genreDropdownOption = (await page.$x("/html/body/div/div/div/div[2]/form/div[2]/section[1]/select/option[17]"))[0];
-    //   await genreDropdownOption.click();
-     
-      //const selector = '/html/body/div/div/div/div[2]/form/div[2]/section[1]/select';
-      //await page.waitForXPath(selector);
-      await page.select(genreDropdown, 'Adult');
+      await page.keyboard.type("2069");
 
+      // await new Promise((resolve) => setTimeout(resolve, 10000));
+      // await page.select("select", "Western");
+
+      await page.waitForXPath(
+        "/html/body/div/div/div/div[2]/form/div[2]/section[2]/input"
+      );
+      const director = (
+        await page.$x(
+          "/html/body/div/div/div/div[2]/form/div[2]/section[2]/input"
+        )
+      )[0];
+      await director.focus();
+      await page.keyboard.type("test director");
+
+      await page.waitForXPath(
+        "/html/body/div/div/div/div[2]/form/div[3]/section[1]/input"
+      );
+      const firstActor = (
+        await page.$x(
+          "/html/body/div/div/div/div[2]/form/div[3]/section[1]/input"
+        )
+      )[0];
+      await firstActor.focus();
+      await page.keyboard.type("test first actor");
+
+      await page.waitForXPath(
+        "/html/body/div/div/div/div[2]/form/div[3]/section[2]/input"
+      );
+      const secondActor = (
+        await page.$x(
+          "/html/body/div/div/div/div[2]/form/div[3]/section[2]/input"
+        )
+      )[0];
+      await secondActor.focus();
+      await page.keyboard.type("test second actor");
 
       // Submit Button
       await page.waitForXPath("/html/body/div/div/div/div[2]/div[2]/button[2]");
@@ -68,7 +111,7 @@ const caseTwoTest = async () => {
     }
   );
 
-  await cluster.execute(
+  await cluster.queue(
     "https://imdb.johnjoyo.dev/api/movies?movie_id=0e9c3edf-7dc4-47af-accb-76a796e8d8b3",
     async ({ data: url }) => {
       const movieData = await axios
@@ -87,19 +130,22 @@ const caseTwoTest = async () => {
 
   await Promise.all(
     dataItems.map(async (item, index) => {
+      delete item.genre;
       console.log(`Read #${index + 1} Data:`);
       console.table(item);
       console.log("\n");
     })
   );
 
+  console.log(`Update movie name to "${selectedName}"`);
   console.log(updateResult);
-  const dataItemsString = dataItems.map((item) => JSON.stringify(item));
   if (dataItems.length === 0) {
     console.log("Transaction failed.");
-  } else if (dataItemsString.every((item) => item === dataItemsString[0]))
-    console.log("Results: All data items match.");
-  else console.log("Results: Data item mismatch. Check transactions.");
+  } else if (dataItems.every((item) => item.name === selectedName)) {
+    console.log("Results: Read matches with update.");
+  } else {
+    console.log("Results: Read and Write mismatch. Check transactions.");
+  }
 
   console.log("\n\n\n");
 };
